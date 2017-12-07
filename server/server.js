a4e05704fdbdbc9a1ec2f365032d51552f4d1e6a
Sungmin Yi <ssuyi@ucdavis.edu>
2017-12-06 18:48:40 -0800

    add connect and socket io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 //setting up path
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io')
+
+
+
 const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 
 //setting up express js
-const express = require('express');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected')
+
+  socket.on('disconnect', () => {
+    console.log('User is disconnected');
+  })
+});
 
 
-app.listen(PORT,() => {
+
+server.listen(PORT,() => {
   console.log(`server is up on ${PORT}`)
 })
